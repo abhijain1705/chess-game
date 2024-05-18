63e58e2d44847360449f10bcbf9a2d3cfe9b36f8
Abhi Jain <abhijain3002@gmail.com>
2024-05-18 17:18:39 +0530

    added page file

--- apps/storefront/src/app/page.tsx
+++ apps/storefront/src/app/page.tsx
@@ -1,30 +1,13 @@
-import { log } from "@repo/logger";
-import { CounterButton, Link } from "@repo/ui";
+import Chess from "../chess/chess";
 
 export const metadata = {
-  title: "Store | Kitchen Sink",
+  title: "Chess Game",
 };
 
 export default function Store(): JSX.Element {
-  log("Hey! This is the Store page.");
-
   return (
-    <div className="container">
-      <h1 className="title">
-        Store <br />
-        <span>Kitchen Sink</span>
-      </h1>
-      <CounterButton />
-      <p className="description">
-        Built With{" "}
-        <Link href="https://turbo.build/repo" newTab>
-          Turborepo
-        </Link>
-        {" & "}
-        <Link href="https://nextjs.org/" newTab>
-          Next.js
-        </Link>
-      </p>
+    <div>
+        <Chess />
     </div>
   );
 }
