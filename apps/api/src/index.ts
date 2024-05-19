3a7cf0c97c1efffdd3f3b73db32ee8322988f655
Abhi Jain <abhijain3002@gmail.com>
2024-05-19 17:51:00 +0530

    added prisma nd login ui

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,26 @@
 import { createServer } from "./server";
 import { log } from "@repo/logger";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const port = process.env.PORT || 3001;
 const server = createServer();
 
-server.listen(port, () => {
-  log(`api running on ${port}`);
+// Function to verify the database connection
+const verifyDbConnection = async () => {
+  try {
+    // Run a simple query to check the connection
+    await prisma.$connect();
+    log("Connected to the database successfully!");
+  } catch (error) {
+    log("Failed to connect to the database");
+    log(error);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+server.listen(port, async () => {
+  await verifyDbConnection(); // Verify the database connection
+  log(`API running on ${port}`);
 });
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import Chess from "../chess/chess";
+import LoginWithTwitter from "../loginWithTwitter/loginWithTwitter";
 
 export default function Web() {
   return (
-    <div>
-      <Chess />
+    <div className="h-[100vh] w-full flex items-center justify-center">
+      <LoginWithTwitter />
     </div>
   );
 }
