503579ddf0d76cc27699c5f97ec6bc7d66c18b92
abhi jain <abhijain3002@gmail.com>
2024-06-16 11:24:51 +0530

    solve bug

--- frontend/src/loginWithTwitter/loginWithTwitter.tsx
+++ frontend/src/loginWithTwitter/loginWithTwitter.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@repo/ui/button";
+"use client";
 import React from "react";
 
 const LoginWithTwitter = () => {
@@ -10,9 +10,9 @@ const LoginWithTwitter = () => {
   };
 
   return (
-    <Button
+    <button
       onClick={api}
-      className="text-white bg-[#000000] rounded-md px-12 py-2 flex items-center justify-center gap-2"
+      className="rounded-md px-12 py-2 flex items-center justify-center gap-2 text-white bg-[#000000] "
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -24,7 +24,7 @@ const LoginWithTwitter = () => {
         <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
       </svg>
       Login with Twitter (X)
-    </Button>
+    </button>
   );
 };
 
