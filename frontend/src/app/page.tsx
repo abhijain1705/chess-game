44dc5210ad67e68ed572e2cfd4842aae2d7bee2c
abhi jain <abhijain3002@gmail.com>
2024-06-16 14:36:40 +0530

    created auth ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,17 @@
+import Chess from "@/chess/chess";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center"></main>
+    <main className="relative">
+      <div className="bg-[#00000080] absolute w-full h-full z-10 flex items-center justify-center">
+        <a
+          href="/login"
+          className="bg-[#ffd59b] cursor-pointer px-[6px] py-[4px] rounded-[6px] border-2 border-solid border-[#000] font-[500] text-[#b26e41]"
+        >
+          Login to play
+        </a>
+      </div>
+      <Chess />
+    </main>
   );
 }
