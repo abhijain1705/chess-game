467474601887740c9f0ba567d9ccd69f99aba82b
abhijain1705 <abhijain3002@gmail.com>
2024-06-17 11:27:18 +0530

    added api routes in frontend nextjs api folder

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <ToastContainer />
       <body className={inter.className}>{children}</body>
     </html>
   );
--- frontend/utils/api_urls.ts
+++ frontend/utils/api_urls.ts
@@ -1,1 +1,10 @@
-const API_URL = process.env.NEXT_PUBLIC_AUTH_API_URL;
+const AUTH_API_URL = process.env.NEXT_PUBLIC_AUTH_API_URL;
+
+export const CHESS_APIS = {
+  AUTH_APIS: {
+    SIGN_UP: AUTH_API_URL + "/signup",
+    LOG_IN: AUTH_API_URL + "/login",
+    FORGOT_PASSWORD: AUTH_API_URL + "/forgot-password",
+    RESET_PASSWORD: AUTH_API_URL + "/reset-password",
+  },
+};
