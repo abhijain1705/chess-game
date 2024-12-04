45da8988686f66db6aede2f7aad1cb1d35afea4b
abhi jain <abhijain3002@gmail.com>
2024-12-04 16:33:44 +0530

    ddfe

---
+++ nest-backend/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
+})
+export class AppModule {}
