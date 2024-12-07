467674bf48312253f28989b7701f244d624e973a
abhi jain <abhijain3002@gmail.com>
2024-12-07 23:25:38 +0530

    ddd

---
+++ nest-backend/src/config/configuration.interface.ts
@@ -0,0 +1,15 @@
+export type ConfigType = {
+  DB_URL: string;
+  PORT: number;
+  EMAIL_USER: string;
+  DB_NAME: string;
+  EMAIL_PASS: string;
+  UI_ROUTE: string;
+  JWT_SECRET: string;
+  REDIS_URL: string;
+  REDIS_HOST: string;
+  REDIS_DB: number;
+  REDIS_PORT: number;
+  REDIS_PRIFIX: string;
+  REDIS_PASSWORD: string;
+};
