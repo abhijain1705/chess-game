48c921c3dfb6340213a331a62b447c4e94376e29
abhi jain <abhijain3002@gmail.com>
2024-12-07 23:23:22 +0530

    ddd

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -1,13 +1,40 @@
+// nestjs import
 import { Module } from '@nestjs/common';
+
+// controller
 import { AppController } from './app.controller';
+
+// services
 import { AppService } from './app.service';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
+
+// modules
+import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './modules/auth/auth.module';
+
+// configuration
+import { Configuration } from './config/configuration';
+import { RedisModule } from './modules/redis/redis.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb', // MongoDB connection
+      url: Configuration.DB_URL, // MongoDB URI from your config
+      database: Configuration.DB_NAME, // Database name
+      useNewUrlParser: true, // Important for MongoDB compatibility
+      useUnifiedTopology: true, // Important for MongoDB compatibility
+      entities: [], // Add your entities here once you create them
+      synchronize: true, // Set to true for dev environment, false for production
+    }),
+    MongooseModule.forRoot(Configuration.DB_URL, {
+      // DB URL
+      dbName: Configuration.DB_NAME, // DB Name
+    }),
+    AuthModule,
+    RedisModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
