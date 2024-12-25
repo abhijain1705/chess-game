c0111e93fe33504224e52b6229f13417aebe2e5a
abhi jain <abhijain3002@gmail.com>
2024-12-25 19:03:03 +0530

    fdsmv

--- nest-backend/src/modules/auth/auth.controller.ts
+++ nest-backend/src/modules/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+// nest imports
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  //   signup
+  //   login
+  //   forgot password
+  // reset password
+
+  constructor(private authService: AuthService) {}
+
+  @Post()
+  login() {}
+
+  @Post()
+  forgotPassword() {}
+
+  @Post()
+  resetPassword() {}
+
+  @Post()
+  signup() {}
+}
